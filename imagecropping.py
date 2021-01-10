b6c094475e1e12d34cbc22eeca9b0c840212177e
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2021-01-09 18:05:27 -0800

    pill images and image manipulation

--- imagecropping.py
+++ imagecropping.py
@@ -2,6 +2,21 @@ import os
 from os import listdir
 from os.path import isfile, join
 import cv2
+import numpy as np
 
-mypath = "pillPictures"
+number = 2
+
+mypath = "pillPictures/" + str(number)
+savepath = "pillPictures/saved"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+img_count = 0
+for file in onlyfiles:
+	img_count = img_count + 1
+	image_path = mypath + "/" + file
+	img = cv2.imread(image_path)
+	#print(np.shape(img))
+	img = img[500:2500,1000:3000]
+	#print(np.shape(img))
+	print(img_count)
+	cv2.imwrite(os.path.join(savepath +"/" + str(number) + "_pill" + "_" +str(img_count)+'.jpg'),img)
