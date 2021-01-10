278c33386a7cbcbceca5572e4cda2aca9460321c
erandeutsch <46551607+erandeutsch@users.noreply.github.com>
2021-01-10 08:34:25 -0800

    Finish the website

--- flaskWebsite/flaskSite.py
+++ flaskWebsite/flaskSite.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask, url_for, render_template
+from flask import Flask, request, url_for, render_template, redirect
 from markupsafe import escape
 
 import firebase_admin
@@ -42,13 +42,29 @@ def writeFirebaseData(name, hour, minute, days):
 
     firebase_admin.delete_app(a)
 
+def deleteFirebaseData(userID, i):
+    cred = credentials.Certificate("serviceAccountKey.json")
+    a = firebase_admin.initialize_app(cred)
 
+    ourDatabase = firestore.client()  
+    collection = ourDatabase.collection('pillboxes')
+    doc = collection.document(userID)
+    userInfo = doc.get().to_dict()
+    userAlarms = userInfo['alarms']
+
+    firebase_admin.delete_app(a)
+
+    userAlarms.pop(int(i))
+
+    doc.update({
+        'alarms': userAlarms
+    })
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Please go to /profile/UserID'
+    return 'Please go to /profile'
 
 #@app.route('/login')
 #def login():
@@ -59,14 +75,24 @@ def index():
 #    return '{}\'s profile'.format(escape(username))
 
 
-
 @app.route('/profile/')
-@app.route('/profile/<userID>')
-def profile(userID=None):
+def profile(userID='1234'):
 
     userAlarms = getFirebaseData(userID)
     return render_template('flaskHTML.html', userAlarms = userAlarms, writing = writeFirebaseData)
 
+@app.route('/profile/', methods=['POST'])
+def profile_post(userID='1234'):
+    if request.form['hidden'] == "creating":
+        name = request.form['name']
+        hour = request.form['hour']
+        minute = request.form['minute']
+
+        writeFirebaseData(name, hour, minute, [1,1,1,1,1,1,1])
+    else:
+        deleteFirebaseData(userID, request.form['hidden'])
+    
+    return redirect(url_for('profile'))
 
 if __name__ == "__main__":
     app.run(debug=True)
